dba365f45fb30f5a580f4eece40632a3d2232561
4C11May <107162641+4C11May@users.noreply.github.com>
2022-09-08 11:06:58 +0800

    Update J100.py

--- J100.py
+++ J100.py
@@ -3,13 +3,13 @@ m = n-1
 x = 1
 for i in range(n+1):
     for j in range(i):
-        print(x, end='') #don't start a new line
+        print(x,'', end='') #don't start a new line
         x = x+1
     print() #new line
     
 while m > 0:
     for j in range(m):
-        print(x, end='')
+        print(x, '', end='')
         x = x+1
     print()
     m = m-1
